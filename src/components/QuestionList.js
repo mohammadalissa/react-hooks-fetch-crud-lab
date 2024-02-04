85eb6392853b19a4e12347850d64c24bf409b18b
Mohammad <mohammed.alissa627@gmail.com>
2024-02-05 00:35:15 +0300

    try

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions,setQuestions] = useState([])
+  useEffect(
+    ()=>{
+      fetch('http://localhost:4000/questions')
+      .then(res=>res.json())
+      .then(data=>setQuestions(data))
+      
+    }
+  ,[])
+  // console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {questions.map((question)=><QuestionItem question={question}></QuestionItem>)}
     </section>
   );
 }
